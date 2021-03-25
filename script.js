6ec940d0f25ceec5fadddc53992dcad392742e45
Melqui Santos <melqsantos96@gmail.com>
2021-03-25 16:39:47 -0300

    :up: Atualizacoes

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+/* Menu lateral */
+
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+/* Video */
+
+var videoPlayer = document.getElementById("VideoPlayer");
+var myVideo = document.getElementById("myVideo");
+
+function stopVideo() {
+    videoPlayer.style.display = "none";
+}
+
+function playVideo(file) {
+    myVideo.src = file;
+    videoPlayer.style.display = "block";
+}
+
+/* Loader */
+
+//<![CDATA[
+$(window).on('load', function() {
+        $('#preloader .inner').fadeOut();
+        $('#preloader').delay(450).fadeOut('slow');
+        $('body').delay(450).css({ 'overflow': 'visible' });
+    })
+    //]]>
